0c04e9f3703f9f8c2e8a3caaed260b87d10b0525
Matheus <matheussantos.quintanilha@gmail.com>
2023-07-11 19:32:01 -0300

    another test

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,90 +1,176 @@
+// import { useState } from 'react';
+
+
+// import { SiConsul } from 'react-icons/si'
+// import { BsPhoneVibrate } from 'react-icons/bs'
+// import { AiOutlineGlobal } from 'react-icons/ai'
+// import { CgMenuGridO } from 'react-icons/cg'
+
+// // Imported images =====>
+// import logo from '../../assets/logo.png'
+
+// const Navbar = () => {
+
+// // Remove the navBar in the small width screens =======>
+//   const [active, setActive] = useState('navBarMenu');
+//   const showNavBar = () => {
+//     setActive('navBarMenu showNavBar')
+//   }
+
+//   const removeNavBar = () => {
+//     setActive('navBarMenu')
+//   }
+
+
+// // Add a background colot to the second Navbar =======>
+//   const [noBg, addBg] = useState('navBarTwo');
+
+//   const addBgColor = () => {
+//     if (window.scrollY >= 10) {
+//       addBg('navBarTwo navbar_With_Bg')
+//     } else {
+//         addBg('navBarTwo')
+//     }
+//   }
+// window.addEventListener('scroll', addBgColor)
+
+//   return (
+//     <div className="navBar flex">
+
+//       <div className="navBarOne flex">
+//         <div>
+//           <SiConsul className='icon'/>
+//         </div>
+
+//         <div className='none flex'>
+//           <li className='flex'> <BsPhoneVibrate  className='icon' /> Support</li>
+//           <li className='flex'> <AiOutlineGlobal className='icon'/> Languages</li>
+//         </div>
+
+//         <div className="atb flex">
+//           <span>Sign In</span>
+//           <span>Sign Out</span>
+//         </div>
+//       </div>
+
+//       <div className={noBg}>
+
+//         <div className="logoDiv">
+//           <img src={logo} className='Logo airplane-logo' />
+//         </div>
+
+//         <div className={active}>
+//           <ul className="menu flex">
+//             <li onClick={removeNavBar} className="listItem">Home</li>
+//             <li onClick={removeNavBar} className="listItem">About</li>
+//             <li onClick={removeNavBar} className="listItem">Offers</li>
+//             <li onClick={removeNavBar} className="listItem">Seats</li>
+//             <li onClick={removeNavBar} className="listItem">Destinations</li>
+//           </ul>
+
+//           <button onClick={removeNavBar} className='btn flex btnOne'>
+//             Contact
+//           </button>
+//         </div>
+
+//         <button className='btn flex btnTwo'>
+//             Contact
+//           </button>
+
+
+//         <div onClick={showNavBar} className='toggleIcon'>
+//           <CgMenuGridO className='icon' />
+//         </div>
+
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Navbar
+
+
 import { useState } from 'react';
-
-
-import { SiConsul } from 'react-icons/si'
-import { BsPhoneVibrate } from 'react-icons/bs'
-import { AiOutlineGlobal } from 'react-icons/ai'
-import { CgMenuGridO } from 'react-icons/cg'
-
-// Imported images =====>
-import logo from '../../assets/logo.png'
+import { SiConsul } from 'react-icons/si';
+import { BsPhoneVibrate } from 'react-icons/bs';
+import { AiOutlineGlobal } from 'react-icons/ai';
+import { CgMenuGridO } from 'react-icons/cg';
+import logo from '../../assets/logo.png';
 
 const Navbar = () => {
-
-// Remove the navBar in the small width screens =======>
   const [active, setActive] = useState('navBarMenu');
   const showNavBar = () => {
-    setActive('navBarMenu showNavBar')
-  }
+    setActive('navBarMenu showNavBar');
+  };
 
   const removeNavBar = () => {
-    setActive('navBarMenu')
-  }
+    setActive('navBarMenu');
+  };
 
-
-// Add a background colot to the second Navbar =======>
   const [noBg, addBg] = useState('navBarTwo');
 
   const addBgColor = () => {
     if (window.scrollY >= 10) {
-      addBg('navBarTwo navbar_With_Bg')
+      addBg('navBarTwo navbar_With_Bg');
     } else {
-        addBg('navBarTwo')
+      addBg('navBarTwo');
     }
-  }
-window.addEventListener('scroll', addBgColor)
+  };
+
+  window.addEventListener('scroll', addBgColor);
 
   return (
     <div className="navBar flex">
-
       <div className="navBarOne flex">
         <div>
-          <SiConsul className='icon'/>
+          <SiConsul className="icon" />
         </div>
-
-        <div className='none flex'>
-          <li className='flex'> <BsPhoneVibrate  className='icon' /> Support</li>
-          <li className='flex'> <AiOutlineGlobal className='icon'/> Languages</li>
+        <div className="none flex">
+          <li className="flex">
+            <BsPhoneVibrate className="icon" /> Support
+          </li>
+          <li className="flex">
+            <AiOutlineGlobal className="icon" /> Languages
+          </li>
         </div>
-
         <div className="atb flex">
           <span>Sign In</span>
           <span>Sign Out</span>
         </div>
       </div>
-
       <div className={noBg}>
-
         <div className="logoDiv">
-          <img src={logo} className='Logo airplane-logo' />
+          <img src={logo} className="Logo airplane-logo" alt="Logo" />
         </div>
-
         <div className={active}>
           <ul className="menu flex">
-            <li onClick={removeNavBar} className="listItem">Home</li>
-            <li onClick={removeNavBar} className="listItem">About</li>
-            <li onClick={removeNavBar} className="listItem">Offers</li>
-            <li onClick={removeNavBar} className="listItem">Seats</li>
-            <li onClick={removeNavBar} className="listItem">Destinations</li>
+            <li onClick={removeNavBar} className="listItem">
+              Home
+            </li>
+            <li onClick={removeNavBar} className="listItem">
+              About
+            </li>
+            <li onClick={removeNavBar} className="listItem">
+              Offers
+            </li>
+            <li onClick={removeNavBar} className="listItem">
+              Seats
+            </li>
+            <li onClick={removeNavBar} className="listItem">
+              Destinations
+            </li>
           </ul>
-
-          <button onClick={removeNavBar} className='btn flex btnOne'>
+          <button onClick={removeNavBar} className="btn flex btnOne">
             Contact
           </button>
         </div>
-
-        <button className='btn flex btnTwo'>
-            Contact
-          </button>
-
-
-        <div onClick={showNavBar} className='toggleIcon'>
-          <CgMenuGridO className='icon' />
+        <button className="btn flex btnTwo">Contact</button>
+        <div onClick={showNavBar} className="toggleIcon">
+          <CgMenuGridO className="icon" />
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     assetsInlineLimit: 0,
     rollupOptions: {
-      external: /^react-icons\/(.*)/,
+      external: ['react-icons'],
     },
   },
 });
