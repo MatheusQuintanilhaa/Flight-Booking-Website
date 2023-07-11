5ada8aeb0f0b5fe6ab970e0df7d1d0f50f8add29
Matheus <matheussantos.quintanilha@gmail.com>
2023-07-11 19:17:45 -0300

    test

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react({
-      resolvers: [
-        {
-          resolveImportMeta(property) {
-            if (property === 'url') {
-              return "'_react-icons/' + id.name";
-            }
-          },
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
   build: {
     assetsInlineLimit: 0,
     rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
+      external: /^react-icons\/(.*)/,
     },
   },
 });
