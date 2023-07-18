23ca4fca3f07a27745016a7ee3fb367253eeb12a
Matheus <matheussantos.quintanilha@gmail.com>
2023-07-18 10:45:34 -0300

    some changes

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,27 +1,27 @@
-import { useEffect } from 'react';
-
-//Imported Icons =========>
-import {HiOutlineLocationMarker} from 'react-icons/hi'
-import {RiAccountPinCircleLine} from 'react-icons/ri'
-import { RxCalendar } from 'react-icons/rx'
-
-// Import AOS ===========>
+import { useEffect, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { RiAccountPinCircleLine } from 'react-icons/ri';
+import { RxCalendar } from 'react-icons/rx';
 import Aos from 'aos';
-import 'aos/dist/aos.css'
+import 'aos/dist/aos.css';
 
 const Search = () => {
+  const [checkInDate, setCheckInDate] = useState(null);
+  const [checkOutDate, setCheckOutDate] = useState(null);
 
-   //UseEffect to set animation duration ===>
   useEffect(() => {
-    Aos.init({duration: 2000})
-  }, [])
+    Aos.init({ duration: 2000 });
+  }, []);
 
-
+  const searchFlights = () => {
+    console.log('Check In Date:', checkInDate);
+    console.log('Check Out Date:', checkOutDate);
+  };
 
   return (
     <div className="search container section">
-      <div data-aos='fade-up' data-aos-duration='2500' className="sectionContainer grid">
-
+      <div data-aos="fade-up" data-aos-duration="2500" className="sectionContainer grid">
         <div className="btns flex">
           <div className="singleBtn">
             <span>Economy</span>
@@ -36,58 +36,59 @@ const Search = () => {
           </div>
         </div>
 
-        <div data-aos='fade-up' data-aos-duration='2000' className="searchInputs flex">
-          {/* Single Input */}
+        <div data-aos="fade-up" data-aos-duration="2000" className="searchInputs flex">
           <div className="singleInput flex">
-            <div className="iconDiv">
-              <HiOutlineLocationMarker className='icon'  />
-            </div>
             <div className="texts">
               <h4>Location</h4>
-              <input type="text" placeholder='Where do you want to go?' />
+              <input type="text" placeholder="Where do you want to go?" />
             </div>
           </div>
 
-          {/* Single Input */}
           <div className="singleInput flex">
             <div className="iconDiv">
-              <RiAccountPinCircleLine className='icon'  />
+              <RiAccountPinCircleLine className="icon" />
             </div>
             <div className="texts">
               <h4>Travelers</h4>
-              <input type="text" placeholder='Add guests' />
+              <input type="text" placeholder="Add guests" />
             </div>
           </div>
 
-          {/* Single Input */}
           <div className="singleInput flex">
             <div className="iconDiv">
-              <RxCalendar className='icon'  />
+              <RxCalendar className="icon" />
             </div>
             <div className="texts">
               <h4>Check In</h4>
-              <input type="text" placeholder='Add date' />
+              <DatePicker
+                selected={checkInDate}
+                onChange={(date) => setCheckInDate(date)}
+                placeholderText="Add date"
+              />
             </div>
           </div>
 
-          {/* Single Input */}
           <div className="singleInput flex">
             <div className="iconDiv">
-              <RxCalendar className='icon'  />
+              <RxCalendar className="icon" />
             </div>
             <div className="texts">
               <h4>Check Out</h4>
-              <input type="text" placeholder='Add date' />
+              <DatePicker
+                selected={checkOutDate}
+                onChange={(date) => setCheckOutDate(date)}
+                placeholderText="Add date"
+              />
             </div>
           </div>
 
-          <button className='btn btnBlock flex'>Search Flight</button>
-
+          <button className="btn btnBlock flex" onClick={searchFlights}>
+            Search Flight
+          </button>
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
